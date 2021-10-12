47243aa8612d6805ffcdbc2abb8145f154d0f902
Ontlametse <dlamezen@gmail.com>
2021-10-12 09:37:17 +0200

    Commit

--- SystemRepository/src/main/java/za/ac/nwu/ac/repo/config/RepositoryConfig.java
+++ SystemRepository/src/main/java/za/ac/nwu/ac/repo/config/RepositoryConfig.java
@@ -1,11 +1,17 @@
 package za.ac.nwu.ac.repo.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
@@ -14,4 +20,5 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @PropertySource(value = "classpath:application-db.properties")
 
 public class RepositoryConfig {
+
 }
